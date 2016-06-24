858332de8e54df6b60857fa7c59eff7d79647727
Giacomo Monni <giacomo.monni98@hotmail.it>
2016-06-24 13:26:06 +0200

    Added liste.py
    First python program using lists

---
+++ liste.py
@@ -0,0 +1,14 @@
+#liste in python
+
+l=[1, 2.5,0.2]
+print(l)
+
+l.sort() #ordine in ordine crescente la lista
+print(l)
+
+l2=['a', 'z', 'n']
+l.extend(l2) #collega un'altra lista (l2) in coda 
+print(l)
+
+l.insert(2,5) #inserisce un nuovo oggetto (5) nell'indice specificato(2=
+print(l)
