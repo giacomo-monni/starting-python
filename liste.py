8b2828f471956956af02546d466a31269390d776
Giacomo Monni <giacomo.monni98@hotmail.it>
2016-06-24 13:29:22 +0200

    English

--- liste.py
+++ liste.py
@@ -1,14 +1,14 @@
-#liste in python
+#lists in python
 
 l=[1, 2.5,0.2]
 print(l)
 
-l.sort() #ordine in ordine crescente la lista
+l.sort() #orders the list in ascending way
 print(l)
 
 l2=['a', 'z', 'n']
-l.extend(l2) #collega un'altra lista (l2) in coda 
+l.extend(l2) #links another list (l2) in queue to a list (l) 
 print(l)
 
-l.insert(2,5) #inserisce un nuovo oggetto (5) nell'indice specificato(2=
+l.insert(2,5) #inserts a new object (5) in the specified index(2)
 print(l)
